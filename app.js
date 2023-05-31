42986348605fb6f0927037c7393f2d329251ef4c
Dauren <d.esmuhanov3@gmail.com>
2023-06-01 01:15:59 +0600

    created routes & controller for each routes && defined a structure for node app

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const morgan = require('morgan');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server-side!");
+const productRouter = require('./routes/productRoutes')
+const userRouter = require('./routes/userRoutes')
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'))
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+app.use('/api/v1/products', productRouter)
+app.use('/api/v1/users', userRouter)
+
+module.exports = app;
---
+++ controllers/productController.js
@@ -0,0 +1,47 @@
+products = [
+  {
+    id: 1,
+    name: 'NVIDIA RTX 2060',
+    price: 1300
+  }
+];
+
+// ROUTE HANDLERS
+exports.getAllProducts = (req, res) => {
+  console.log(req.requestTime)
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: products.length,
+    data: {
+      products
+    }
+  })
+}
+
+exports.getProduct = (req, res) => {
+  console.log(req.params)
+
+  res.send('Your request has been handled! 😘')
+}
+
+exports.createProduct = (req, res) => {
+  console.log(req.body)
+  res.send('Done')
+}
+
+exports.updateProduct = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      product: 'Product has been updated successfully!'
+    }
+  })
+}
+
+exports.deleteProduct = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+}
