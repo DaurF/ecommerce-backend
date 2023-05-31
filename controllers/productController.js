92412c47d467933d445e89c27adad25c760dfc94
Dauren <d.esmuhanov3@gmail.com>
2023-06-01 05:25:46 +0600

    implemented crud operations for products collection

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,45 +1,92 @@
-products = [
-  {
-    id: 1,
-    name: 'NVIDIA RTX 2060',
-    price: 1300
-  }
-];
+const Product = require('../models/productModel')
 
 // ROUTE HANDLERS
-exports.getAllProducts = (req, res) => {
-  console.log(req.requestTime)
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: products.length,
-    data: {
-      products
-    }
-  })
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      status: 'success',
+      results: products.length,
+      data: {
+        products
+      }
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
 }
 
-exports.getProduct = (req, res) => {
-  res.send('Your request has been handled! 😘')
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product
+      }
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
 }
 
-exports.createProduct = (req, res) => {
-  console.log(req.body)
-  res.send('Done')
+exports.createProduct = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body)
+    res.status(201).json({
+      status: 'success',
+      data: {
+        product: newProduct
+      }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent'
+    })
+  }
 }
 
-exports.updateProduct = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      product: 'Product has been updated successfully!'
-    }
-  })
-}
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product
+      }
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
 
-exports.deleteProduct = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  })
-}
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
+
+};
