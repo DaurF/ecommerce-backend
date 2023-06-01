09e36dd530799a22a84edf51b15fa6879a8efeec
Dauren <d.esmuhanov3@gmail.com>
2023-06-01 19:25:27 +0600

    Срефакторил фильтрацию, пагинацию, сортировку и т.д.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModel')
+const APIFeatures = require('../utils/apiFeatures')
 
 exports.aliasTopProducts = (req, res, next) => {
   req.query.limit = '5';
@@ -10,48 +11,13 @@ exports.aliasTopProducts = (req, res, next) => {
 
 exports.getAllProducts = async (req, res) => {
   try {
-    // Строим запрос, исключяя определенные поля по типу page, sort...
-
-    // 1A) Фильтр
-    const queryObj = {...req.query};
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el])
-
-    // 1B) Фильтр со сравнительными операторами
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-    let query = Product.find(JSON.parse(queryStr));
-
-    // 2) Сортировка
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt')
-    }
-
-    // 3) Ограничение полей
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) Пагинация
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numProducts = await Product.countDocuments();
-      if (skip >= numProducts) throw new Error('This page does not exist');
-    }
-
-    // Выполняем запрос и получаем  результат запросы
-    const products = await query;
+    // Выполняем запрос и получаем  результат
+    const features = new APIFeatures(Product.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const products = await features.query;
 
     // Отправляем ответ клиенту
     res.status(200).json({
