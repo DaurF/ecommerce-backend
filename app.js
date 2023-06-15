0099e931518f4620f2e5fd047d582cfd3f5df4b5
Dauren <d.esmuhanov3@gmail.com>
2023-06-16 02:38:02 +0600

    Добавил функции для CRUD операций всех routes и добавил endpoint для информации о текущем пользователе

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
@@ -30,13 +38,6 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
-
-app.use(
-  cors({
-    origin: 'http://127.0.0.1:5173',
-    credentials: true,
-  })
-);
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
